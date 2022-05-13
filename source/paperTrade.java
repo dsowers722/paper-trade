83749df014f1eabfbd7a0fb747b8abf793294921
Daniel S <danielsowers3@gmail.com>
2022-05-13 13:07:23 -0400

    Take user inputs

--- source/paperTrade.java
+++ source/paperTrade.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class paperTrade {
+    public static String username;
+    public static String password;
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Username:");
+        username = scan.nextLine();
+        System.out.println("Password:");
+        password = scan.nextLine();
     }
 }
