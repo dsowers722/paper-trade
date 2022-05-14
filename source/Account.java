c7e03cf7686f6cb1e039e3ed5caf894ba3256d95
Daniel S <danielsowers3@gmail.com>
2022-05-14 03:15:01 -0400

    Changed name of method

--- source/Account.java
+++ source/Account.java
@@ -1,11 +1,13 @@
 public class Account {
+
     String username;
     String password;
+
     public Account(String username, String password) {
         this.username = username;
         this.password = password;
     }
-    public void Start() {
-        System.out.println("Start");
+    public void SaveInfo() {
+        
     }
 }
