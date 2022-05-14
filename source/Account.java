26bf21b1e4dd87387de18595a559ee1664e2d412
Daniel S <danielsowers3@gmail.com>
2022-05-14 03:25:35 -0400

    Added file writing

--- source/Account.java
+++ source/Account.java
@@ -1,3 +1,8 @@
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Account {
 
     String username;
@@ -8,6 +13,14 @@ public class Account {
         this.password = password;
     }
     public void SaveInfo() {
-        
+        try {
+            FileWriter fw = new FileWriter("userinfo.txt", true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(username + " " + password );
+            bw.newLine();
+            bw.close();
+        } catch (IOException e) {
+            System.out.println("Error with file");
+        }
     }
 }
