c41e118c625521bfa3115b5a77199d7e73c30e1f
Daniel S <danielsowers3@gmail.com>
2022-05-17 08:16:28 -0400

    Minor changes

--- source/paperTrade.java
+++ source/paperTrade.java
@@ -1,24 +1,62 @@
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class paperTrade {
-    private static ArrayList<Account> accounts = new ArrayList<Account>();
+    private static ArrayList<Account> accounts = new ArrayList<>();
+    static Boolean exists;
     public static void main(String[] args) {
-        Boolean accountExists;
         Scanner scan = new Scanner(System.in);
         System.out.println("Username:");
         String username = scan.nextLine();
         System.out.println("Password:");
         String password = scan.nextLine();
-        Account account = new Account(username, password);
         ReadData();
+        checkForAccount(username);
+        if (!exists) {
+            Account account = new Account(username, password);
+            WriteData(account);
+        }
     }
 
     public static void WriteData(Account accountWrite) {
-
+        try {
+            FileOutputStream fileOutStream = new FileOutputStream("Accounts.ser", true);
+            ObjectOutputStream objectOutStream = new ObjectOutputStream(fileOutStream);
+            objectOutStream.writeObject(accountWrite);
+            objectOutStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void ReadData() {
+        try {
+            FileInputStream fileInStream = new FileInputStream("Accounts.ser");
+            ObjectInputStream objectInSteam = new ObjectInputStream(fileInStream);
+            while (true) {
+                try {
+                    accounts.add((Account) objectInSteam.readObject());
+                } catch (EOFException eofException) {
+                    break;
+                }
+            }
+            objectInSteam.close();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
 
+    public static boolean checkForAccount(String username) {
+        for (Account account : accounts) {
+            if (account.username.equals(username)) {
+                System.out.println("Account with the given username already exists!\n" +
+                                   "Please try again.");
+                exists = true;
+                return true;
+            }
+        }
+        exists = false;
+        return false;
     }
 }
